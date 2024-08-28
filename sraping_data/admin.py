0e2be16c95d49f878fa4d2eee467010692fd6992
baranovr <rusipbox@gmail.com>
2024-08-28 16:45:43 +0300

    registered models in admin.py

--- sraping_data/admin.py
+++ sraping_data/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from sraping_data.models import Vacancy, Technology, HistoricalData
 
-# Register your models here.
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", "company", "location", "technology", "experience", "date_posted", "views_popularity", "source"
+    )
+    search_fields = ("title", "company", "location", "technology")
+    list_filter = ("location", "technology", "date_posted", "source")
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ("name", "vacancy_count")
+    search_fields = ("name",)
+
+
+@admin.register(HistoricalData)
+class HistoricalDataAdmin(admin.ModelAdmin):
+    list_display = ("technology", "date", "vacancy_count")
+    list_filter = ("date",)
+    search_fields = ("technology__name",)
