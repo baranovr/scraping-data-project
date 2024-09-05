ae70500561fadb18239aa3a69bcfc1c75124bb6a
baranovr <rusipbox@gmail.com>
2024-09-05 19:59:45 +0300

    changed fileds in JobLiting model

--- scraping_data/models.py
+++ scraping_data/models.py
@@ -8,8 +8,8 @@ class JobListing(models.Model):
     years_of_experience = models.CharField(max_length=50, null=True, blank=True)
     salary = models.CharField(max_length=50, null=True, blank=True)
     english_level = models.CharField(max_length=50, null=True, blank=True)
-    technologies = models.CharField(max_length=255)
-    views_popularity = models.CharField(max_length=555, null=True, blank=True)
+    technologies = models.CharField(max_length=555)
+    seniority_level = models.CharField(max_length=55, null=True, blank=True)
     date_posted = models.CharField(max_length=150, null=True, blank=True)
     source = models.CharField(max_length=50, null=True, blank=True)
 
@@ -18,23 +18,3 @@ class JobListing(models.Model):
 
     class Meta:
         verbose_name_plural = "Vacancies"
-
-
-class Technology(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    vacancy_count = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class HistoricalData(models.Model):
-    date = models.DateField(auto_now_add=True)
-    technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
-    vacancy_count = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.technology.name} on {self.date}"
-
-    class Meta:
-        verbose_name_plural = "Historical Data"
